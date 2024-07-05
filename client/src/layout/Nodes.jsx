80feebad93e4f4ce39f0f94855d297f5e079002c
supertick <grog@myrobotlab.org>
2024-07-05 07:55:01 -0700

    wip

--- client/src/layout/Nodes.jsx
+++ client/src/layout/Nodes.jsx
@@ -17,7 +17,7 @@ const Nodes = () => {
   const getRepoUrl = useStore((state) => state.getRepoUrl)
   const getTypeImage = useStore((state) => state.getTypeImage)
 
-  const registry = useStore((state) => state.services)
+  const registry = useStore((state) => state.registry)
   const serviceArray = Object.values(registry)
   const fgRef = useRef()
 
@@ -128,126 +128,143 @@ const Nodes = () => {
   }
 
   return (
-    <SplitPane split="vertical" minSize={200} defaultSize="70%">
-      <div>
-        {/* Radio buttons to select display mode */}
-        <div>
-          <label>
-            <input
-              type="radio"
-              value="services"
-              checked={mode === "services"}
-              onChange={(e) => setMode(e.target.value)}
+    <>
+      <style>{`
+        html, body, #root, .SplitPane {
+          height: 100%;
+          margin: 0;
+          padding: 0;
+        }
+        .pane {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          height: 100%;
+        }
+      `}</style>
+      <SplitPane split="vertical" minSize={200} defaultSize="70%">
+        <div className="pane">
+          {/* Radio buttons to select display mode */}
+          <div>
+            <label>
+              <input
+                type="radio"
+                value="services"
+                checked={mode === "services"}
+                onChange={(e) => setMode(e.target.value)}
+              />
+              Services
+            </label>
+            <label>
+              <input type="radio" value="ids" checked={mode === "ids"} onChange={(e) => setMode(e.target.value)} />
+              IDs
+            </label>
+            <label>
+              <input type="radio" value="hosts" checked={mode === "hosts"} onChange={(e) => setMode(e.target.value)} />
+              Hosts
+            </label>
+          </div>
+          {/* Force-directed graph to display nodes and links */}
+          <div>
+            <ForceGraph2D
+              ref={fgRef}
+              graphData={{ nodes, links }}
+              nodeAutoColorBy="group"
+              linkWidth={2}
+              nodeLabel="name"
+              onNodeClick={handleNodeClick}
+              onNodeDragEnd={() => {
+                stopSimulation()
+              }}
+              linkDirectionalArrowLength={6}
+              linkDirectionalArrowRelPos={1}
+              linkCurvature={0.25}
+              linkCanvasObjectMode={() => "after"}
+              linkCanvasObject={(link, ctx, globalScale) => {
+                const MAX_FONT_SIZE = 4
+                const LABEL_NODE_MARGIN = 1.5
+                const start = link.source
+                const end = link.target
+
+                // Calculate the midpoint of the link
+                const midPos = Object.assign(
+                  ...["x", "y"].map((c) => ({
+                    [c]: start[c] + (end[c] - start[c]) / 2 // Calculate middle point
+                  }))
+                )
+
+                const relLink = { x: end.x - start.x, y: end.y - start.y }
+
+                const textPos = Object.assign(
+                  ...["x", "y"].map((c) => ({
+                    [c]: midPos[c] - relLink[c] / 2
+                  }))
+                )
+
+                const linkLabel = `${link.source.name} -> ${link.target.name}`
+
+                // Estimate font size to fit in link length
+                ctx.font = "1px Sans-Serif"
+                const fontSize = Math.min(MAX_FONT_SIZE, 8 / ctx.measureText(linkLabel).width)
+                ctx.font = `${fontSize}px Sans-Serif`
+                ctx.fillStyle = "rgba(0, 0, 0, 0.8)"
+                ctx.textAlign = "center"
+                ctx.textBaseline = "middle"
+
+                // Draw text label along the edge
+                ctx.save()
+                ctx.translate(textPos.x, textPos.y)
+                ctx.rotate(Math.atan2(relLink.y, relLink.x))
+                ctx.fillText(linkLabel, 0, 0)
+                ctx.restore()
+              }}
+              linkDirectionalParticles={1}
+              linkDirectionalParticleSpeed={0.01}
+              linkDirectionalParticleWidth={2}
+              nodeCanvasObject={(node, ctx, globalScale) => {
+                const img = new Image()
+                img.src = getTypeImage(node.id)
+                const nameLabel = node.name
+                const fontSize = 12 / globalScale
+                const size = 12
+
+                // Draw the node image and label
+                img.onload = () => {
+                  ctx.save()
+                  ctx.clearRect(node.x - size / 2, node.y - size / 2, size, size)
+                  // Draw rounded edge outline
+                  ctx.beginPath()
+                  ctx.drawImage(img, node.x - size / 2, node.y - size / 2, size, size)
+
+                  ctx.font = `${fontSize}px Sans-Serif`
+                  ctx.fillStyle = "black"
+                  ctx.textAlign = "center"
+                  ctx.textBaseline = "middle"
+                  ctx.fillText(nameLabel, node.x, node.y + 18)
+                }
+
+                if (img.complete) {
+                  ctx.save()
+                  ctx.clearRect(node.x - size / 2, node.y - size / 2, size, size)
+                  // Draw rounded edge outline
+                  ctx.beginPath()
+                  ctx.drawImage(img, node.x - size / 2, node.y - size / 2, size, size)
+
+                  ctx.font = `${fontSize}px Sans-Serif`
+                  ctx.fillStyle = "black"
+                  ctx.textAlign = "center"
+                  ctx.textBaseline = "middle"
+                  ctx.fillText(nameLabel, node.x, node.y + 18)
+                }
+              }}
             />
-            Services
-          </label>
-          <label>
-            <input type="radio" value="ids" checked={mode === "ids"} onChange={(e) => setMode(e.target.value)} />
-            IDs
-          </label>
-          <label>
-            <input type="radio" value="hosts" checked={mode === "hosts"} onChange={(e) => setMode(e.target.value)} />
-            Hosts
-          </label>
+          </div>
         </div>
-        {/* Force-directed graph to display nodes and links */}
-        <div>
-          <ForceGraph2D
-            ref={fgRef}
-            graphData={{ nodes, links }}
-            nodeAutoColorBy="group"
-            linkWidth={2}
-            nodeLabel="name"
-            onNodeClick={handleNodeClick}
-            onNodeDragEnd={() => {
-              stopSimulation()
-            }}
-            linkDirectionalArrowLength={6}
-            linkDirectionalArrowRelPos={1}
-            linkCurvature={0.25}
-            linkCanvasObjectMode={() => "after"}
-            linkCanvasObject={(link, ctx, globalScale) => {
-              const MAX_FONT_SIZE = 4
-              const LABEL_NODE_MARGIN = 1.5
-              const start = link.source
-              const end = link.target
-
-              // Calculate the midpoint of the link
-              const midPos = Object.assign(
-                ...["x", "y"].map((c) => ({
-                  [c]: start[c] + (end[c] - start[c]) / 2 // Calculate middle point
-                }))
-              )
-
-              const relLink = { x: end.x - start.x, y: end.y - start.y }
-
-              const textPos = Object.assign(
-                ...["x", "y"].map((c) => ({
-                  [c]: midPos[c] - relLink[c] / 2
-                }))
-              )
-
-              const linkLabel = `${link.source.name} -> ${link.target.name}`
-
-              // Estimate font size to fit in link length
-              ctx.font = "1px Sans-Serif"
-              const fontSize = Math.min(MAX_FONT_SIZE, 8 / ctx.measureText(linkLabel).width)
-              ctx.font = `${fontSize}px Sans-Serif`
-              ctx.fillStyle = "rgba(0, 0, 0, 0.8)"
-              ctx.textAlign = "center"
-              ctx.textBaseline = "middle"
-
-              // Draw text label along the edge
-              ctx.save()
-              ctx.translate(textPos.x, textPos.y)
-              ctx.rotate(Math.atan2(relLink.y, relLink.x))
-              ctx.fillText(linkLabel, 0, 0)
-              ctx.restore()
-            }}
-            linkDirectionalParticles={1}
-            linkDirectionalParticleSpeed={0.01}
-            linkDirectionalParticleWidth={2}
-            nodeCanvasObject={(node, ctx, globalScale) => {
-              const img = new Image()
-              img.src = getTypeImage(node.id)
-              const nameLabel = node.name
-              const fontSize = 12 / globalScale
-              const size = 12
-
-              // Draw the node image and label
-              img.onload = () => {
-                ctx.save()
-                ctx.clearRect(node.x - size / 2, node.y - size / 2, size, size)
-                // Draw rounded edge outline
-                ctx.beginPath()
-                ctx.drawImage(img, node.x - size / 2, node.y - size / 2, size, size)
-
-                ctx.font = `${fontSize}px Sans-Serif`
-                ctx.fillStyle = "black"
-                ctx.textAlign = "center"
-                ctx.textBaseline = "middle"
-                ctx.fillText(nameLabel, node.x, node.y + 18)
-              }
-
-              if (img.complete) {
-                ctx.save()
-                ctx.clearRect(node.x - size / 2, node.y - size / 2, size, size)
-                // Draw rounded edge outline
-                ctx.beginPath()
-                ctx.drawImage(img, node.x - size / 2, node.y - size / 2, size, size)
-
-                ctx.font = `${fontSize}px Sans-Serif`
-                ctx.fillStyle = "black"
-                ctx.textAlign = "center"
-                ctx.textBaseline = "middle"
-                ctx.fillText(nameLabel, node.x, node.y + 18)
-              }
-            }}
-          />
+        <div className="pane">
+          <ServicePane service={selectedService} />
         </div>
-      </div>
-      <ServicePane service={selectedService} />
-    </SplitPane>
+      </SplitPane>
+    </>
   )
 }
 
