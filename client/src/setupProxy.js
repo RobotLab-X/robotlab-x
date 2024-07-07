3f7c890b6a73bf2578c3537ee7bac700ad71ce47
supertick <grog@myrobotlab.org>
2024-07-06 20:54:29 -0700

    proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,45 +4,20 @@ module.exports = function (app) {
   app.use(
     "/api/service",
     createProxyMiddleware({
-      // target: "http://localhost:8888",
-      target: "https://192.168.0.7:8443",
+      target: "http://localhost:8888",
+      // target: "https://192.168.0.7:8443",
       changeOrigin: true,
       // ws: true,
       secure: false,
     })
   )
-  app.use(
-    "/offer",
-    createProxyMiddleware({
-      target: "http://192.168.0.7:8080", // webcam.py
-      changeOrigin: true,
-      // ws: true,
-      secure: false,
-    })
-  )
-  // Proxy WebSocket connections to a remote server running on port 8080
   // app.use(
-  //   "/api/messages/*",
+  //   "/offer",
   //   createProxyMiddleware({
-  //     target: "wss://localhost:8443/api/messages",
-  //     // target: "ws://localhost:8443/api/messages",
+  //     target: "http://192.168.0.7:8080", // webcam.py
   //     changeOrigin: true,
-  //     ws: true,
+  //     // ws: true,
   //     secure: false,
-  //     logLevel: "debug",
-  //   })
-  // )
-
-  // dumb "hot swap" dev server related
-  // app.use(
-  //   "/ws",
-  //   createProxyMiddleware({
-  //     target: "wss://localhost:8443/api/messages",
-  //     // target: "ws://localhost:8443/api/messages",
-  //     changeOrigin: true,
-  //     ws: true,
-  //     secure: false,
-  //     logLevel: "debug",
   //   })
   // )
 }
