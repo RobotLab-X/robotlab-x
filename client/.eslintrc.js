db8bd2edb4117fd599e7bd48527adc0cd21b182d
supertick <grog@myrobotlab.org>
2024-07-15 16:13:14 -0700

    more lint rules

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,7 +1,13 @@
 module.exports = {
   // other ESLint configurations
+  plugins: [
+    "react-hooks"
+    // other plugins
+  ],
   rules: {
-    "no-unused-vars": "warn",
-    "react-hooks/exhaustive-deps": "warn"
+    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+    "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
+    "no-unused-vars": "warn"
+    // other rules
   }
 }
