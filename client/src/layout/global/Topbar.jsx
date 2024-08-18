786ad3825b6b7c26a7248a029fcc33a719a1a67f
GroG <grog@myrobotlab.org>
2024-08-17 17:03:32 -0700

    topbar closer

--- client/src/layout/global/Topbar.jsx
+++ client/src/layout/global/Topbar.jsx
@@ -1,149 +1,118 @@
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined"
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined"
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined"
-import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined"
-import StatusList from "../../components/StatusList"
-
+import PowerSettingsNewIcon from "@mui/icons-material/PowerSettingsNew"
 import SearchIcon from "@mui/icons-material/Search"
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined"
 import {
   Box,
   Button,
-  Checkbox,
   Dialog,
   DialogActions,
   DialogContent,
+  DialogContentText,
   DialogTitle,
-  FormControlLabel,
   IconButton,
-  InputAdornment,
   TextField,
   Typography,
   useTheme
 } from "@mui/material"
-import { useProcessedMessage } from "hooks/useProcessedMessage"
+import InputAdornment from "@mui/material/InputAdornment"
 import React, { useContext, useEffect, useState } from "react"
 import { useStore } from "store/store"
-import useServiceSubscription from "store/useServiceSubscription"
-import { ColorModeContext, tokens } from "../../theme"
+import { ColorModeContext } from "../../theme"
 
 const Topbar = () => {
-  // data from ipc main process
   const [versions, setVersions] = useState({ appVersion: "", chrome: "", node: "", electron: "" })
+  const [filter, setFilter] = useState("")
+  const [settingsDialogOpen, setSettingsDialogOpen] = useState(false)
+  const [shutdownDialogOpen, setShutdownDialogOpen] = useState(false)
 
   const theme = useTheme()
-  const colors = tokens(theme.palette.mode)
   const colorMode = useContext(ColorModeContext)
-  const [filter, setFilter] = useState("")
   const remoteId = useStore((state) => state.defaultRemoteId)
-  const debug = useStore((state) => state.debug)
-  const setDebug = useStore((state) => state.setDebug)
   const sendTo = useStore((state) => state.sendTo)
 
-  const serviceMsg = useServiceSubscription(`runtime@${remoteId}`)
-  const service = useProcessedMessage(serviceMsg)
-
-  const [dialogOpen, setDialogOpen] = useState(false)
-
-  const statusListStyle = {
-    position: "absolute",
-    bottom: 160,
-    marginBottom: "16px",
-    width: "100%"
-  }
-
   useEffect(() => {
-    // Fetch version information from the electron API
-    // BE AWARE ! - window.electron does not exist in browser !!!
     const versionInfo = window?.electron?.getVersions()
     setVersions(versionInfo)
   }, [])
 
-  const handleDialogOpen = () => {
-    setDialogOpen(true)
+  const handleShutdown = () => {
+    setShutdownDialogOpen(false)
+    console.log("Shutdown initiated")
   }
 
-  const handleDialogClose = () => {
-    setDialogOpen(false)
+  const handleRestart = () => {
+    setShutdownDialogOpen(false)
+    sendTo("runtime", "restart")
   }
-
-  const handleCheckboxChange = (event) => {
-    setDebug(event.target.checked)
-    sendTo("runtime", "setDebug", event.target.checked)
-  }
-
-  const host = (service && Object.values(service?.hosts)[0]) || {}
 
   return (
     <Box display="flex" justifyContent="space-between" alignItems="center" p={2}>
       <Box display="flex" flexGrow={1} alignItems="center">
-        <Box width={280} mr={2}>
-          <TextField
-            fullWidth
-            variant="outlined"
-            placeholder="Search..."
-            onChange={(e) => setFilter(e.target.value)}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <SearchIcon />
-                </InputAdornment>
-              )
-            }}
-          />
-        </Box>
-        <Typography variant="h4" component="span">
-          {remoteId} {host?.platform} {service?.pkg?.version}
+        <TextField
+          fullWidth
+          variant="outlined"
+          placeholder="Search..."
+          onChange={(e) => setFilter(e.target.value)}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            )
+          }}
+        />
+        <Typography variant="h4" component="span" ml={2}>
+          {remoteId}
         </Typography>
       </Box>
 
       <Box display="flex" alignItems="center">
         <IconButton onClick={colorMode.toggleColorMode}>
-          {theme?.palette?.mode === "dark" ? <DarkModeOutlinedIcon /> : <LightModeOutlinedIcon />}
+          {theme.palette.mode === "dark" ? <DarkModeOutlinedIcon /> : <LightModeOutlinedIcon />}
         </IconButton>
         <IconButton>
           <NotificationsOutlinedIcon />
         </IconButton>
-        <IconButton onClick={handleDialogOpen}>
+        <IconButton onClick={() => setSettingsDialogOpen(true)}>
           <SettingsOutlinedIcon />
         </IconButton>
-        <IconButton>
-          <PersonOutlinedIcon />
+        <IconButton onClick={() => setShutdownDialogOpen(true)}>
+          <PowerSettingsNewIcon />
         </IconButton>
       </Box>
 
-      <Dialog open={dialogOpen} onClose={handleDialogClose}>
+      <Dialog open={settingsDialogOpen} onClose={() => setSettingsDialogOpen(false)}>
         <DialogTitle>Settings</DialogTitle>
         <DialogContent>
-          <div>
-            {/* // BE AWARE ! - window.electron does not exist in browser !!! */}
-            <p>App version: {versions?.appVersion}</p>
-            <p>Chrome version: {versions?.chrome}</p>
-            <p>Node.js version: {versions?.node}</p>
-            <p>Electron version: {versions?.electron}</p>
-          </div>
-
-          <FormControlLabel
-            control={<Checkbox checked={debug} onChange={handleCheckboxChange} name="debug" />}
-            label="Debug"
-          />
+          <Typography>App version: {versions?.appVersion}</Typography>
+          <Typography>Chrome version: {versions?.chrome}</Typography>
+          <Typography>Node.js version: {versions?.node}</Typography>
+          <Typography>Electron version: {versions?.electron}</Typography>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleDialogClose} color="primary">
+          <Button onClick={() => setSettingsDialogOpen(false)} color="primary">
             Close
           </Button>
         </DialogActions>
       </Dialog>
 
-      {debug && (
-        <>
-          {" "}
-          <br />
-          <Box sx={statusListStyle}>
-            <StatusList />
-          </Box>
-        </>
-      )}
+      <Dialog open={shutdownDialogOpen} onClose={() => setShutdownDialogOpen(false)}>
+        <DialogTitle>Shutdown or Restart</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Do you want to shutdown or restart the system?</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleShutdown} color="error">
+            Shutdown
+          </Button>
+          <Button onClick={handleRestart} color="primary">
+            Restart
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Box>
   )
 }
